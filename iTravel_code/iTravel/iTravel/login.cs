7c7c44dfeaf2175ce8f1d13ec5fb72adc547d56c
gaojunping <1832331653@qq.com>
2017-05-04 23:22:31 +0800

    gaojunp
    gaojunping_code 20170504

---
+++ iTravel_code/iTravel/iTravel/login.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace iTravel
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void labelX1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonX2__LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            this.Hide();
+
+            signin zhuce = new signin();
+            zhuce.Show();
+
+        }//显示注册界面
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            string User, Pwd; //用户名，密码
+            bool flagshow = false;//用来标注登录名是否存在于数据库
+            string strConnect = "Data Source=CAI-PC\\SQLEXPRESS;Initial Catalog=MyData1;Persist Security Info=True;User ID=sa;Password=******"; SqlConnection conConnection = new SqlConnection(strConnect); conConnection.Open();
+
+            string cmd = "select 用户名,密码,email from 用户";
+
+            SqlCommand com = new SqlCommand(cmd, conConnection); SqlDataReader reader = com.ExecuteReader();
+
+            while (reader.Read())//从数据库读取用户信息
+            {
+
+                User = reader["用户名"].ToString();
+
+                Pwd = reader["密码"].ToString();
+
+                if (User.Trim() == textBoxX1.Text & Pwd.Trim() == textBoxX2.Text)
+                {
+
+                    flagshow = true; //用户名存在于数据库，则为true
+
+                }
+
+            }
+
+            reader.Close();
+
+            conConnection.Close();
+
+            if (flagshow == true)
+            {
+                showMainForm();//用户存在，返回登录界面
+
+            }
+
+            else
+            {
+
+                MessageBox.Show("用户不存在或密码错误！", "提示");
+
+                return;
+
+            }
+
+        }
+
+        private void showMainForm()
+        {
+            this.Close();
+            Form1 main = new Form1();
+            main.Show();
+        }
+
+        private void textBoxX2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            signin zhuce = new signin( );
+            zhuce.Show( );
+
+        }
+
+        private void buttonX3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ iTravel_code/iTravel/iTravel/signin.Designer.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace iTravel
+{
+    partial class signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(signin));
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.labelX6 = new DevComponents.DotNetBar.LabelX();
+            this.labelX7 = new DevComponents.DotNetBar.LabelX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textBoxX2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textBoxX3 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            this.labelX1.BackColor = System.Drawing.Color.LightSteelBlue;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelX1.Location = new System.Drawing.Point(116, 75);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(380, 47);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "iTravel|Become a traveller";
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelX2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelX2.Location = new System.Drawing.Point(142, 162);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(75, 23);
+            this.labelX2.TabIndex = 1;
+            this.labelX2.Text = "用户名";
+            this.labelX2.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelX3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelX3.Location = new System.Drawing.Point(142, 205);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(75, 23);
+            this.labelX3.TabIndex = 2;
+            this.labelX3.Text = "设置密码";
+            this.labelX3.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelX4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelX4.Location = new System.Drawing.Point(142, 247);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(75, 23);
+            this.labelX4.TabIndex = 3;
+            this.labelX4.Text = "确认密码";
+            this.labelX4.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // labelX5
+            // 
+            // 
+            // 
+            // 
+            this.labelX5.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelX5.Location = new System.Drawing.Point(408, 160);
+            this.labelX5.Name = "labelX5";
+            this.labelX5.Size = new System.Drawing.Size(88, 23);
+            this.labelX5.TabIndex = 4;
+            this.labelX5.Text = "请输入用户名";
+            this.labelX5.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // labelX6
+            // 
+            // 
+            // 
+            // 
+            this.labelX6.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelX6.Location = new System.Drawing.Point(409, 248);
+            this.labelX6.Name = "labelX6";
+            this.labelX6.Size = new System.Drawing.Size(87, 23);
+            this.labelX6.TabIndex = 5;
+            this.labelX6.Text = "重新输入密码";
+            this.labelX6.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // labelX7
+            // 
+            // 
+            // 
+            // 
+            this.labelX7.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX7.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelX7.Location = new System.Drawing.Point(408, 205);
+            this.labelX7.Name = "labelX7";
+            this.labelX7.Size = new System.Drawing.Size(88, 23);
+            this.labelX7.TabIndex = 6;
+            this.labelX7.Text = "区分大小写";
+            this.labelX7.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonX1.Location = new System.Drawing.Point(256, 319);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(78, 33);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 7;
+            this.buttonX1.Text = "注册";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX1.Location = new System.Drawing.Point(223, 162);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(180, 21);
+            this.textBoxX1.TabIndex = 8;
+            // 
+            // textBoxX2
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX2.Border.Class = "TextBoxBorder";
+            this.textBoxX2.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX2.Location = new System.Drawing.Point(223, 207);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(180, 21);
+            this.textBoxX2.TabIndex = 9;
+            // 
+            // textBoxX3
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX3.Border.Class = "TextBoxBorder";
+            this.textBoxX3.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX3.Location = new System.Drawing.Point(223, 248);
+            this.textBoxX3.Name = "textBoxX3";
+            this.textBoxX3.Size = new System.Drawing.Size(180, 21);
+            this.textBoxX3.TabIndex = 10;
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.ManagerColorTint = System.Drawing.Color.Transparent;
+            this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2010Silver;
+            this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154))))));
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 422);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // signin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 422);
+            this.Controls.Add(this.textBoxX3);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.labelX7);
+            this.Controls.Add(this.labelX6);
+            this.Controls.Add(this.labelX5);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "signin";
+            this.Text = "signin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+         bool  flagRegister;//定义标志位，确认用户注册
+         string strConnect = "Data Source=CAI-PC\\SQLEXPRESS;Initial Catalog=MyData1;Persist Security Info=True;User ID=sa;Password=******";
+         //连接数据库字符串
+         private void buttonX1_Click(object sender, EventArgs e)
+        {
+          if ((textBoxX1.Text.Length >= 4) && (textBoxX1.Text.Length <= 12) &&
+
+           (textBoxX2.Text.Length >= 6) && (textBoxX3.Text.Length==textBoxX2 .Text .Length ))
+        {
+
+        flagRegister = true;
+
+        }
+         else
+        {
+            if ((textBoxX1.Text.Length < 4) || (textBoxX1.Text.Length > 12))
+            {
+                if (textBoxX1.Text.Length == 0)
+                {
+                    MessageBox.Show("请输入用户名", "提示");
+                    return;
+                }
+
+                else
+                {
+                    MessageBox.Show("用户名长度不在约定范围内,请重新输入！", "提示");
+                    return;
+                }
+
+            }
+            else
+            {
+
+                if (textBoxX2.Text.Length < 6 || textBoxX2.Text.Length < 6)
+                {
+
+                    MessageBox.Show("密码长度不足6位，请重新输入！", "提示");
+
+                    return;
+
+                }
+                if (textBoxX3.Text != textBoxX2.Text)
+                {
+                    MessageBox.Show("密码输入不一致，请重新输入", "提示");
+                }
+            }
+        }
+        if (UserFlag == true)
+        {
+
+            MessageBox.Show("用户已经存在，请重新输入！");
+
+            return;
+
+        }
+        if (flagRegister == true) //确认用户注册后，把用户写入数据库
+        {
+
+            SqlConnection conConnection = new SqlConnection(strConnect);
+
+            conConnection.Open();
+
+            string cmd = "insert into 用户(用户名,密码) values ('" + textBoxX1.Text + "'," + "'" + textBoxX2.Text + "') ";
+
+            SqlCommand com = new SqlCommand(cmd, conConnection);
+
+            com.ExecuteNonQuery();
+
+            conConnection.Close();
+
+            MessageBox.Show("注册成功！点击确定，返回登录界面。", "提示");
+
+            this.Close();
+           
+        }
+
+      }
+        public bool UserFlag; //定义标志位，来确认用户是否存在
+
+        private void textBoxX1_TextChanged(object sender, EventArgs e) 
+       {
+
+        SqlConnection conConnection = new SqlConnection(strConnect); conConnection.Open();
+
+        string cmd = "select 用户名 from 用户";
+
+        SqlCommand com = new SqlCommand(cmd, conConnection); SqlDataReader readerUser = com.ExecuteReader();
+
+        while (readerUser.Read())
+
+       {
+
+         if (textBoxX1.Text == readerUser["用户名"].ToString().Trim()) {
+
+         MessageBox .Show("用户已存在，请重新输入！","提示");
+
+        UserFlag = true;
+
+        return;
+
+         }
+
+          else if (textBoxX1.Text != readerUser["用户名"].ToString().Trim()) 
+        {
+
+          MessageBox .Show ("恭喜你，该用户名可以使用。", "提示");
+
+          UserFlag = false;
+
+        }
+
+       }
+    }
+
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.LabelX labelX5;
+        private DevComponents.DotNetBar.LabelX labelX6;
+        private DevComponents.DotNetBar.LabelX labelX7;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX2;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX3;
+        private DevComponents.DotNetBar.StyleManager styleManager1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
